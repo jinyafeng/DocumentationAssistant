051bb7867ad700046b322486395557d85ebf2b65
Yafeng Jin <pjin@datto.com>
2020-08-23 15:44:12 +0800

    - Add interface tester.

---
+++ DocumentationAssistant/DocumentationAssistant.Test/InterfaceUnitTests.cs
@@ -0,0 +1,104 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace DocumentationAssistant.Test
+{
+	/// <summary>
+	/// The interface unit test.
+	/// </summary>
+	[TestClass]
+	public class InterfaceUnitTest : CodeFixVerifier
+	{
+		/// <summary>
+		/// The test code.
+		/// </summary>
+		private const string TestCode = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+	interface IInterfaceTester
+	{
+	}
+}";
+
+		/// <summary>
+		/// The test fix code.
+		/// </summary>
+		private const string TestFixCode = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    /// <summary>
+    /// The interface tester.
+    /// </summary>
+    interface IInterfaceTester
+	{
+	}
+}";
+
+		/// <summary>
+		/// Nos diagnostics show.
+		/// </summary>
+		/// <param name="testCode">The test code.</param>
+		[DataTestMethod]
+		[DataRow("")]
+		public void NoDiagnosticsShow(string testCode)
+		{
+			this.VerifyCSharpDiagnostic(testCode);
+		}
+
+		/// <summary>
+		/// Shows diagnostic and fix.
+		/// </summary>
+		/// <param name="testCode">The test code.</param>
+		/// <param name="fixCode">The fix code.</param>
+		/// <param name="line">The line.</param>
+		/// <param name="column">The column.</param>
+		[DataTestMethod]
+		[DataRow(TestCode, TestFixCode, 8,12)]
+		public void ShowDiagnosticAndFix(string testCode, string fixCode, int line, int column)
+		{
+			var expected = new DiagnosticResult
+			{
+				Id = InterfaceAnalyzer.DiagnosticId,
+				Message = InterfaceAnalyzer.MessageFormat,
+				Severity = DiagnosticSeverity.Warning,
+				Locations =
+					new[] {
+							new DiagnosticResultLocation("Test0.cs", line, column)
+						}
+			};
+
+			this.VerifyCSharpDiagnostic(testCode, expected);
+
+			this.VerifyCSharpFix(testCode, fixCode);
+		}
+
+		/// <summary>
+		/// Gets c sharp code fix provider.
+		/// </summary>
+		/// <returns>A CodeFixProvider.</returns>
+		protected override CodeFixProvider GetCSharpCodeFixProvider()
+		{
+			return new InterfaceCodeFixProvider();
+		}
+
+		/// <summary>
+		/// Gets c sharp diagnostic analyzer.
+		/// </summary>
+		/// <returns>A DiagnosticAnalyzer.</returns>
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			return new InterfaceAnalyzer();
+		}
+	}
+}
