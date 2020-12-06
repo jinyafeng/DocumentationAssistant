c0a5d47120c74bffeb98ff80997a891ab8e6496f
Yafeng Jin <pjin@datto.com>
2020-12-06 18:57:36 +0800

    Support configuration feature for #3.

---
+++ DocumentatioAssistant.Configuration/OptionsPage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentatioAssistant.Configuration
+{
+	public class OptionsPage : DialogPage
+	{
+		[Category("Options")]
+		[DisplayName("OnlyEnabledToPublishMembers")]
+		[Description("Only public members will be checked when the value is true.")]
+		public bool EnabledToPublishMembersOnly { get; set; } = false;
+	}
+}
