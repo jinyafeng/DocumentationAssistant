bb6e4a7b5aab6a26e2a934c575689d5f3d082687
jinyafeng <435643383@qq.com>
2020-12-07 21:00:41 +0800

    - Support configuration.

---
+++ DocumentationAssistant/DocumentationAssistant.Config/OptionsPage.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System.ComponentModel;
+
+namespace DocumentationAssistant.Config
+{
+	public class OptionsPage : DialogPage
+	{
+		[Category("Options")]
+		[DisplayName("OnlyEnabledToPublishMembers")]
+		[Description("Only public members will be checked when the value is true.")]
+		public bool EnabledToPublishMembersOnly { get; set; } = false;
+	}
+}
--- DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
+++ DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
@@ -1,7 +1,37 @@
-﻿namespace DocumentationAssistant.Helper
+﻿using DocumentationAssistant.Config;
+using Microsoft.VisualStudio.Shell;
+
+namespace DocumentationAssistant.Helper
 {
 	public static class Configuration
 	{
-		public static bool IsEnabledForPublishMembersOnly = false;
+		private static IOptionsService _optionsService = null;
+
+		public static bool IsEnabledForPublishMembersOnly
+		{
+			get
+			{
+				if (_optionsService == null)
+				{
+					try
+					{
+						_optionsService = Package.GetGlobalService(typeof(SOptionsService)) as IOptionsService;
+					}
+					catch (System.Exception)
+					{
+						// Do unit test will cause the exception: "Message=Could not load type 'System.Windows.Threading.Dispatcher' from assembly 'WindowsBase, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'."
+						// Don't know why this cause error.
+					}
+				}
+
+				var result = false;
+				if (_optionsService != null)
+				{
+					result = _optionsService.IsEnabledToPublicMembersOnly();
+				}
+
+				return result;
+			}
+		}
 	}
 }
