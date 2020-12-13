c290e4abbff4d543fed0691d5d527022c0d02893
jinyafeng <435643383@qq.com>
2020-12-13 16:45:14 +0800

    - Revert previous changes

--- DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
+++ DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
@@ -1,37 +1,7 @@
-﻿using DocumentatioAssistant.Configuration;
-using Microsoft.VisualStudio.Shell;
-
-namespace DocumentationAssistant.Helper
+﻿namespace DocumentationAssistant.Helper
 {
 	public static class Configuration
 	{
-		private static IOptionsService _optionsService = null;
-
-		public static bool IsEnabledForPublishMembersOnly
-		{
-			get
-			{
-				if (_optionsService == null)
-				{
-					try
-					{
-						_optionsService = Package.GetGlobalService(typeof(SOptionsService)) as IOptionsService;
-					}
-					catch (System.Exception)
-					{
-						// Do unit test will cause the exception: "Message=Could not load type 'System.Windows.Threading.Dispatcher' from assembly 'WindowsBase, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'."
-						// Don't know why this cause error.
-					}
-				}
-
-				var result = false;
-				if (_optionsService != null)
-				{
-					result = _optionsService.IsEnabledToPublicMembersOnly();
-				}
-
-				return result;
-			}
-		}
+		public static bool IsEnabledForPublishMembersOnly = false;
 	}
 }
