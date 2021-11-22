5721e6ab4b092242cdeff7e74da69ced0e812da9
jinyafeng <9931183+chuxin6@user.noreply.gitee.com>
2021-11-22 21:55:54 +0800

    Remove config project as it is not worked actually.

--- DocumentationAssistant/DocumentationAssistant/PropertyAnalyzer.cs
+++ DocumentationAssistant/DocumentationAssistant/PropertyAnalyzer.cs
@@ -1,83 +1,78 @@
-﻿using System.Collections.Immutable;
-using System.Linq;
-using DocumentationAssistant.Helper;
+﻿using DocumentationAssistant.Helper;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace DocumentationAssistant
 {
-	/// <summary>
-	/// The property analyzer.
-	/// </summary>
-	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	public class PropertyAnalyzer : DiagnosticAnalyzer
-	{
-		/// <summary>
-		/// The title.
-		/// </summary>
-		private const string Title = "The property must have a documentation header.";
+    /// <summary>
+    /// The property analyzer.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PropertyAnalyzer : DiagnosticAnalyzer
+    {
+        /// <summary>
+        /// The title.
+        /// </summary>
+        private const string Title = "The property must have a documentation header.";
 
-		/// <summary>
-		/// The category.
-		/// </summary>
-		private const string Category = DocumentationHeaderHelper.Category;
+        /// <summary>
+        /// The category.
+        /// </summary>
+        private const string Category = DocumentationHeaderHelper.Category;
 
-		/// <summary>
-		/// The diagnostic id.
-		/// </summary>
-		public const string DiagnosticId = "PropertyDocumentationHeader";
+        /// <summary>
+        /// The diagnostic id.
+        /// </summary>
+        public const string DiagnosticId = "PropertyDocumentationHeader";
 
-		/// <summary>
-		/// The message format.
-		/// </summary>
-		public const string MessageFormat = Title;
+        /// <summary>
+        /// The message format.
+        /// </summary>
+        public const string MessageFormat = Title;
 
-		/// <summary>
-		/// The diagnostic descriptor rule.
-		/// </summary>
-		private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+        /// <summary>
+        /// The diagnostic descriptor rule.
+        /// </summary>
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
 
-		/// <summary>
-		/// Gets the supported diagnostics.
-		/// </summary>
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+        /// <summary>
+        /// Gets the supported diagnostics.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-		/// <summary>
-		/// Initializes.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		public override void Initialize(AnalysisContext context)
-		{
-			context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.PropertyDeclaration);
-		}
+        /// <summary>
+        /// Initializes.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.PropertyDeclaration);
+        }
 
-		/// <summary>
-		/// Analyzes node.
-		/// </summary>
-		/// <param name="context">The context.</param>
-		private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
-		{
-			PropertyDeclarationSyntax node = context.Node as PropertyDeclarationSyntax;
+        /// <summary>
+        /// Analyzes node.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            PropertyDeclarationSyntax node = context.Node as PropertyDeclarationSyntax;
 
-			if (Configuration.IsEnabledForPublishMembersOnly && PrivateMemberVerifier.IsPrivateMember(node))
-			{
-				return;
-			}
+            DocumentationCommentTriviaSyntax commentTriviaSyntax = node
+                .GetLeadingTrivia()
+                .Select(o => o.GetStructure())
+                .OfType<DocumentationCommentTriviaSyntax>()
+                .FirstOrDefault();
 
-			DocumentationCommentTriviaSyntax commentTriviaSyntax = node
-				.GetLeadingTrivia()
-				.Select(o => o.GetStructure())
-				.OfType<DocumentationCommentTriviaSyntax>()
-				.FirstOrDefault();
+            if (commentTriviaSyntax != null && CommentHelper.HasComment(commentTriviaSyntax))
+            {
+                return;
+            }
 
-			if (commentTriviaSyntax != null && CommentHelper.HasComment(commentTriviaSyntax))
-			{
-				return;
-			}
-
-			context.ReportDiagnostic(Diagnostic.Create(Rule, node.Identifier.GetLocation()));
-		}
-	}
+            context.ReportDiagnostic(Diagnostic.Create(Rule, node.Identifier.GetLocation()));
+        }
+    }
 }
