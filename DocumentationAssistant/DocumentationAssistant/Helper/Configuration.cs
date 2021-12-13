c258a406fc10f85d8c12ea3f7e965dba4839a87e
jinyafeng <pjin@datto.com>
2021-12-13 10:01:07 +0800

    Enable for public members only.

--- DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
+++ DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
@@ -5,6 +5,6 @@
 		/// <summary>
 		/// Gets a value indicating whether the tool is enabled for only public members.
 		/// </summary>
-		public static bool IsEnabledForPublicMembersOnly => false;
+		public static bool IsEnabledForPublicMembersOnly => true;
 	}
 }
