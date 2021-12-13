8e04f1c4be53f06b854aaa2923caceec392c1783
jinyafeng <pjin@datto.com>
2021-12-13 20:19:05 +0800

    - enable for public members only,

--- DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
+++ DocumentationAssistant/DocumentationAssistant/Helper/Configuration.cs
@@ -2,9 +2,6 @@
 {
 	public static class Configuration
 	{
-		/// <summary>
-		/// Gets a value indicating whether the tool is enabled for only public members.
-		/// </summary>
 		public static bool IsEnabledForPublicMembersOnly => true;
 	}
 }
